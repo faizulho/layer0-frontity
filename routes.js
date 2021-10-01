e956428f045eb7304dc7e908975dc818448633b0
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-10-02 03:19:12 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,4 +1,25 @@
 const { Router } = require('@layer0/core/router')
-const { nextRoutes } = require('@layer0/frontity')
 
-module.exports = new Router().use(frontityRoutes)
+module.exports = new Router()
+  .get('/service-worker.js', ({ serviceWorker }) => {
+    // serve the service worker built by webpack
+    serviceWorker('dist/service-worker.js')
+  })
+  .get('/p/:productId', ({ cache }) => {
+    // cache products for one hour at edge and using the service worker
+    cache({
+      edge: {
+        maxAgeSeconds: 60 * 60,
+        staleWhileRevalidateSeconds: 60 * 60,
+      },
+      browser: {
+        maxAgeSeconds: 0,
+        serviceWorkerSeconds: 60 * 60,
+      },
+    })
+    proxy('origin')
+  })
+  .fallback(({ proxy }) => {
+    // serve all unmatched URLs from the origin backend configured in layer0.config.js
+    proxy('origin')
+  })
