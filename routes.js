3beb252640732cbd46ad69e248307f88926c2f2c
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-10-02 03:39:54 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,25 +1,13 @@
-const { Router } = require('@layer0/core/router')
+const createNextPlugin = require('@layer0/next/router/createNextPlugin')
 
-module.exports = new Router()
-  .get('/service-worker.js', ({ serviceWorker }) => {
-    // serve the service worker built by webpack
-    serviceWorker('dist/service-worker.js')
-  })
-  .get('/p/:productId', ({ cache }) => {
-    // cache products for one hour at edge and using the service worker
-    cache({
-      edge: {
-        maxAgeSeconds: 60 * 60,
-        staleWhileRevalidateSeconds: 60 * 60,
-      },
-      browser: {
-        maxAgeSeconds: 0,
-        serviceWorkerSeconds: 60 * 60,
-      },
-    })
-    proxy('origin')
-  })
-  .fallback(({ proxy }) => {
-    // serve all unmatched URLs from the origin backend configured in layer0.config.js
-    proxy('origin')
-  })
+module.exports = app => {
+  const { nextMiddleware, renderNext } = createNextPlugin(app)
+
+  return new Router()
+    .destination("legacy",
+      new Router().fallback(({ proxy }) => proxy("legacy"))
+    )
+    .destination("pwa",
+      new Router().use(nextMiddleware)
+    )
+}
